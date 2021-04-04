a93de8b5c68dff0037c6ecd2c8c63b72cce7711a
Tselmen Tugsbayar <tugsbayartselmen@gmail.com>
2021-04-04 17:03:34 +0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
+import {useDispatch} from "react-redux";
 
-function App() {
+import {getPosts} from './actions/posts'
+import memories from './images/memories.png'
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from './styles';
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h3" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt={"memories"} height={50}/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify={"space-between"} alignItems={"stretch"} spacing={3}
+                className={classes.mainContainer}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId}/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
+};
 
 export default App;
